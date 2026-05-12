8004705c337aadc1ab4ed10b862ea3963edbd5a1
“beata-dan” <beata.danilevskaya@gmail.com>
2026-05-12 15:30:18 +0100

    Updated yaml file

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 COPY package*.json /
 RUN npm ci
 COPY . .
-ENV PORT=5000
+ENV PORT=3000
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import dayjs from 'dayjs'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const DATA_DIR = path.join(import.meta.dirname, 'data')
 const WEATHER_FILE = path.join(DATA_DIR, 'weather.json')
